7278dd22480df6d23509325e4ed1de7134d3da72
Alain Normandin <4203914+anormandin@users.noreply.github.com>
2023-07-12 15:31:15 -0400

    Config modified

--- nvim/after/plugin/catppuccin.rc.lua
+++
@@ -1,8 +0,0 @@
-local status, it = pcall(require, "catppuccin")
-if (not status) then return end
-
-vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
-it.setup()
-vim.api.nvim_command "colorscheme catppuccin"
-
-
