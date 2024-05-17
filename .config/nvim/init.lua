aee234aa136290da7b48f6911ba6229f02ec8128
Alain Normandin <4203914+anormandin@users.noreply.github.com>
2024-05-17 11:22:19 -0400

    added some config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,26 @@
+require "user.launch"
+require "user.options"
+require "user.keymaps"
+require "user.autocmds"
+
+spec "user.colorscheme"
+spec "user.devicons"
+spec "user.whichkey"
+spec "user.treesitter"
+spec "user.mason"
+spec "user.lspconfig"
+spec "user.null-ls"
+spec "user.telescope"
+spec "user.nvimtree"
+spec "user.lualine"
+spec "user.bufdelete"
+spec "user.cmp"
+spec "user.comment"
+
+-- spec "user.neoscroll"
+
+
+
+spec "user.copilot"
+
+require "user.lazy"
---
+++ .config/nvim/lua/user/mason.lua
@@ -0,0 +1,36 @@
+local M = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+}
+
+M.servers = {
+  "lua_ls",
+  "cssls",
+  "html",
+  "tsserver",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "marksman",
+}
+
+function M.config()
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>lI"] = { "<cmd>Mason<cr>", "Mason Info" },
+  }
+
+  require("mason").setup {
+    ui = {
+      border = "rounded",
+    },
+  }
+  require("mason-lspconfig").setup {
+    ensure_installed = M.servers,
+  }
+end
+
+return M
