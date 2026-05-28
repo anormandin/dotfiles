3dea550e3955b9c800e6830526d7bdd8791bd4f9
Alain Normandin <alain.normandin@neosapiens.com>
2026-05-28 16:57:39 -0400

    feat: add stow aliases and fix vimconfig to point at init.lua

--- home/.config/zsh/aliases.zsh
+++ home/.config/zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias bucc="npm install -g @anthropic-ai/claude-code"
 alias neo="cd ~/Documents/dev.nosync/neosapiens/"
-alias vimconfig="nvim ~/.config/nvim/init.vim"
+alias vimconfig="nvim ~/.config/nvim/init.lua"
 
 # Better ls
 alias ls='eza --icons'
@@ -14,3 +14,10 @@ compdef eza=ls
 alias grep='rg --color=auto'
 alias diff='diff --color=auto'
 alias df='df -h'
+
+# --- dotfiles / GNU Stow (package: home) ---
+alias dot='cd ~/dotfiles'
+alias stow-on='stow -v -t ~ -d ~/dotfiles home'      # create/refresh symlinks
+alias stow-off='stow -D -v -t ~ -d ~/dotfiles home'  # remove symlinks
+alias restow='stow -R -v -t ~ -d ~/dotfiles home'    # re-stow (picks up newly added files)
+alias stow-dry='stow -n -v -t ~ -d ~/dotfiles home'  # preview, change nothing
